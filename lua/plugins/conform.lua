e532396623bbb49991e6b4b07254eeb45faa3541
Gabriel Gizotti <gabriel@gizotti.com>
2025-09-20 20:54:31 +1000

    Update Lazy

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -23,10 +23,22 @@ return {
             RUBOCOP_DISABLE_PENDING_COPS = "true",
           },
         },
+        erb_lint = {
+          command = "bundle",
+          args = {
+            "exec",
+            "erb_lint",
+            "--autocorrect",
+            "--stdin",
+          },
+          stdin = true,
+          cwd = require("conform.util").root_file({ "Gemfile", ".erb-lint.yml" }),
+          exit_codes = { 0, 1 },
+        },
       },
       formatters_by_ft = {
         ruby = { "rubocop" },
-        eruby = {},
+        eruby = { "erb_lint" },
       },
     },
   },
